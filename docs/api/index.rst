3af9e65d0114a5faebed1dd85742bd0362a5cdd7
siva-shiba <shiba4525@gmail.com>
2026-03-10 17:10:40 +0900

    Update docs style

--- docs/api/index.rst
+++ docs/api/index.rst
@@ -5,3 +5,20 @@ API リファレンス
    :members:
    :undoc-members:
    :show-inheritance:
+
+モジュール一覧
+--------------
+
+.. toctree::
+   :maxdepth: 1
+
+   zunda.dataset
+   zunda.classification
+   zunda.cross_validation
+   zunda.cv_adapters
+   zunda.dataset_adapters
+   zunda.data_augmentation
+   zunda.predictor
+   zunda.losses
+   zunda.callbacks
+   zunda.utils
--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,13 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",  # NumPy/Google style docstrings
+    "sphinx_rtd_theme",
 ]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_theme = "furo"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_title = "zunda ドキュメント"
 
